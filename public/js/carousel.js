910377be0954edf05e1b2710ffb90946ea96f62b
Nick Parsons <clapynick@gmail.com>
2020-07-14 23:08:16 +1000

    Product carousel (with jump bug)

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,6 +1,6 @@
 $(function(){
-  const $owl = $('.owl-carousel');
-  $owl.owlCarousel({
+  const $owlProfile = $('.owl-profile-carousel');
+  $owlProfile.owlCarousel({
     loop: true,
     margin: 20,
     stagePadding: 30,
@@ -16,6 +16,28 @@ $(function(){
       },
       1000:{
           items: 4
+      }
+    }
+  });
+
+  $owlProducts = $(".owl-products-carousel");
+  $owlProducts.owlCarousel({
+    loop: true,
+    center: true,
+    smartSpeed: 1000,
+    nav: true,
+    dots: false,
+    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    mouseDrag: false,
+    responsive:{
+      0:{
+        items: 1,
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items: 3
       }
     }
   });
