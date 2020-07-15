f6c53c30738c520da3b898887dc245cababb49c9
Nick Parsons <clapynick@gmail.com>
2020-07-15 22:59:36 +1000

    Disabled animations on mobile.

--- public/libs/scroll-animate/scroll-config.js
+++ public/libs/scroll-animate/scroll-config.js
@@ -1,6 +1,7 @@
 // https://github.com/michalsnik/aos#-how-to-use-it
 $(function() {
   AOS.init({
+    disable: 'phone',
     mirror: true, /* apply animation on scroll out */
     duration: 500,
   });
