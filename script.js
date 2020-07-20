6c614b83f90779d3c9d646b93ab792bdea6ec765
Nick Parsons <clapynick@gmail.com>
2020-07-20 22:46:08 +1000

    Sent form data to server.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -10,5 +11,23 @@ httpServer.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies (for POST)
+  extended: true
+})); 
+
 app.use(express.static(path.join(__dirname, './public')));
 app.get('/', express.static(path.join(__dirname, './public')));
+app.post('/sendMail', (req, res) => {
+  const {firstName, lastName, email, emailBody} = req.body;
+  let error = Object.values(req.body).some(val => !val.trim());
+
+  if(!error) {
+    // try and send email...
+
+    // if error, set error to false.
+  }
+
+  res.json({
+    error
+  });
+})
