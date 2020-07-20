4d5e22454f65fca4a3d0b456d147ce2bd1d95e7d
Nick Parsons <clapynick@gmail.com>
2020-07-20 22:46:59 +1000

    Added post express handler for email data

--- public/js/email.js
+++ public/js/email.js
@@ -3,8 +3,11 @@ $(function() {
     e.preventDefault(); // stop page refresh
     // Validate Recaptcha
 
-    const $data = $("#contact-form").serialize();
-    // Send data to server to be emailed using $.ajax
-
+    const data = $(this).serialize();
+    // Send data to server to be emailed using $.post
+    // Validity will be reported back and checked on the sever.
+    $.post("/sendMail", data, function(response) {
+      console.log(response);
+    });
   });
 });
