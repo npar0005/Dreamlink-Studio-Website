5091e3585a1abdeb13d1ede0abe525c6caddc6da
Nick Parsons <clapynick@gmail.com>
2020-07-24 23:09:37 +1000

    Added page scroll buttons

---
+++ public/js/scroll.js
@@ -0,0 +1,19 @@
+let offset = 100;
+let arrowShown = false;
+$(function() {
+  $('[data-scroll="true"]').click("click", function() {
+    offset = $(this).data("offset") || offset;
+    $("html, body").animate({ scrollTop: $($(this).data("to")).offset().top - offset }, "slow");
+  });
+});
+
+$(window).scroll(function() {
+  const scroll  = $(this).scrollTop();
+  if (scroll > 20 && !arrowShown) {
+    $("#scroll-top").stop().fadeIn(600);
+    arrowShown = !arrowShown;
+  } else if (scroll <= 20 && arrowShown) {
+    $("#scroll-top").stop().fadeOut(500);
+    arrowShown = !arrowShown;
+  }
+});
