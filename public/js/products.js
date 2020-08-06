ec0c22827cf0515ce942ee1ec4c252bd670da57b
Nick Parsons <clapynick@gmail.com>
2020-08-06 21:41:03 +1000

    Clicking open expandable now closes it

--- public/js/products.js
+++ public/js/products.js
@@ -1,16 +1,22 @@
 $(function() {
   $("[data-open='true']").css({display: 'block'});
+  let prevSelector = '';
   $("[data-expand]").click(function() {
     const selector = $(this).data('expand');
     const $info = $(selector);
     const $info_wrapper = $info.closest(".product-info-wrapper");
+    const wrapper_open = $info_wrapper.attr('data-open') === "true";
     
-    if(!$info_wrapper.data('open')) {
-      $info_wrapper.slideToggle();
-      $info_wrapper.data('open', true);
+    if(!wrapper_open || prevSelector === selector) {
+      $info_wrapper.stop().slideToggle();
+      $info_wrapper.attr('data-open', !wrapper_open);
+    } 
+    // If user click on a different expandable than the one they're currently viewing
+    if(prevSelector !== selector) { 
+      prevSelector = selector;
+      $('.show').hide().removeClass('show');
+      $info.stop().fadeIn();
+      $info.addClass('show');
     }
-    $('.show').hide().removeClass('show');
-    $info.stop().fadeIn();
-    $info.addClass('show');
   });
 });
