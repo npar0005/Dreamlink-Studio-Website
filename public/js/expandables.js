a2ddb18825ad88a9151f664f5404e8e76dec7c04
Nick Parsons <clapynick@gmail.com>
2020-08-13 20:13:53 +1000

    Fixed animation bugs

--- public/js/expandables.js
+++ public/js/expandables.js
@@ -3,22 +3,24 @@ class Expandable {
     this._$expandableElem = $(elem);
     this._$expandableElem.data('expandableIndex', i);
     this._isOpen = false;
+
+    this._expandableSpeed = 500;
+    this._showSpeed = 400;
   }
 
   showExpandable() {
     if(!this._isOpen) {
-      this._$expandableElem.stop().stop().slideDown();
+      this._$expandableElem.stop().slideDown(this._expandableSpeed);
       this._isOpen = true;
     }
   }
 
   showContent(contentSelector) {
-    const speed = 400;
     // If the content which is currently shown is different to the content we want to show, then change it, else don't change it
     if(this._displayedContentSelector !== contentSelector) {
       const $toShowContent = $(contentSelector);
       $('.show', this._$expandableElem).hide().removeClass('show');
-      $toShowContent.stop().fadeIn(this._isOpen ? speed : 0);
+      $toShowContent.stop().fadeIn(this._isOpen ? this._showSpeed : 0);
       $toShowContent.addClass('show'); // show class marks the expanded info
       this._displayedContentSelector = contentSelector;
     } 
@@ -26,7 +28,7 @@ class Expandable {
 
   closeExpandable() {
     if(this._isOpen) {
-      this._$expandableElem.stop().slideUp();
+      this._$expandableElem.stop().slideUp(this._expandableSpeed);
       this._isOpen = false;
       this._displayedContentSelector = "";
     }
